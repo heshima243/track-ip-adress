6bbf6e971b64b3d010615c5d5d2ebb345ac2b1c5
heshima243 <heshimajulien.official@gmail.com>
2024-08-10 18:17:34 +0200

    first commit

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
 const express = require('express');
-const fs = require('fs');
+const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
 
+// URL de connexion à MongoDB
+const mongoUrl = 'mongodb+srv://heshimajulienofficial:gZo66bAOKJBetFSQ@localisation.st4rgvh.mongodb.net/localisation?retryWrites=true&w=majority';
+
+// Connexion à MongoDB avec Mongoose
+mongoose.connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('Connected successfully to MongoDB'))
+.catch(err => {
+    console.error('Failed to connect to MongoDB', err);
+    process.exit(1); // Arrêtez le serveur si la connexion échoue
+});
+
+// Définir un schéma pour les adresses IP
+const ipSchema = new mongoose.Schema({
+    ip: String,
+    date: { type: Date, default: Date.now }
+});
+
+// Créer un modèle basé sur le schéma
+const IpLog = mongoose.model('IpLog', ipSchema);
+
 app.use(express.static('public'));
 
-// Middleware pour récupérer l'adresse IP de l'utilisateur
-app.use((req, res, next) => {
+// Middleware pour enregistrer l'adresse IP de l'utilisateur
+app.use(async (req, res, next) => {
     const userIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    const log = `IP: ${userIp}, Date: ${new Date().toISOString()}\n`;
-    
-    // Enregistrer l'IP dans un fichier (par exemple: logs/ips.txt)
-    fs.appendFileSync(path.join(__dirname, 'logs', 'ips.txt'), log);
+    console.log('User IP:', userIp); // Ajout d'une journalisation
 
-    console.log(log);
-    next();
+    try {
+        const log = new IpLog({ ip: userIp });
+        const savedLog = await log.save(); // Enregistrer le document dans MongoDB
+        console.log(`IP: ${userIp} logged successfully with ID: ${savedLog._id}`);
+    } catch (err) {
+        console.error('Failed to log IP address:', err);
+    } finally {
+        next();
+    }
 });
 
 // Route pour afficher une page simple
